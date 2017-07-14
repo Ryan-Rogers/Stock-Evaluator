129e5554c81bd2579bd36537ef67be7fa664341e
Andrew <snickgerz@gmail.com>
2017-07-14 17:10:49 -0500

    return list of stock tickers

--- screener.py
+++ screener.py
@@ -4,34 +4,35 @@ from yahoo_finance import Share
 
 def menu():
     """ show menu """
+    print ('\n')
     print (30 * '-')
     print ("   M A I N - M E N U")
     print (30 * '-')
-    print ("1. Import Tickers (.txt)")
+    print ("1. Import Tickers (tickers.txt)")
     print ("2. Manual Input")
     print ("3. Run Analysis")
     print ("4. Exit program")
     print (30 * '-')
     try:
         selection = int(raw_input('Enter your choice [1-4] : '))
+        options = {1: import_tickers, 2: manual_tickers, 3: run_analysis, 4: exit_program}
+        options[selection]()
     except ValueError:
         print("Please enter only integers please!")
         exit()
-    options = {1: import_tickers, 2: manual_tickers, 3: run_analysis, 4: exit_program}
-    options[selection]()
 
 def import_tickers():
     """import tickers from tickers.txt in working directory"""
     if os.path.isfile('tickers.txt') == True:
-        file = open('tickers.txt', 'r')
-        values = file.read()
-        print values
+        with open('tickers.txt') as f:
+            content = f.readlines()
+        content = [x.strip() for x in content]
+        print content
         print("import successful")
-        file.close()
-        return values
+        f.close()
+        return content
     else:
         print("no text file found")
-
 
 def manual_tickers():
     """manually enter tickers"""
@@ -48,7 +49,6 @@ def exit_program():
     """exit program"""
     print("Exiting Program")
     exit()
-
 
 if __name__ == '__main__':
     while True:

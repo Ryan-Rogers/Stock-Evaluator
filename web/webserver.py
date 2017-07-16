f13bb8ac10b68438546a1673ef18694db3bc6147
Ryan Rogers <github@ryanrogers.org>
2017-07-16 12:21:23 -0500

    Added new (empty) search page for future functionality.

--- web/webserver.py
+++ web/webserver.py
@@ -1,47 +1,47 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import jsonify
 import config
-from datetime import datetime
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-
 heading = dict()
+
 
 @app.route("/")
 def main():
+    print("config.LOCAL_DNS_PORT:", config.LOCAL_DNS_PORT)
+    return str(config.LOCAL_DNS_PORT)
 
-	print("config details")
 
-	print(config.LOCAL_DNS_PORT)
-
-	return str(config.LOCAL_DNS_PORT)
-
-@app.route("/stocks/")
+@app.route("/stocks")
 def stocks():
-	ip = config.LOCAL_DNS_HOST
-	return render_template('index.html',ip=ip)
+    ip = config.LOCAL_DNS_HOST
+    return render_template('index.html', ip=ip)
 
-#@app.route('/_stuff', methods=['GET'])
-#def stuff():
-	# global heading
-	# if request.method == 'GET':
-	# 	if heading:
-	# 		print heading['value']
-	# 		return (heading['value'])
-	# 	else :
-	# 		blank = ""
-	# 		return blank
+
+@app.route("/search")
+def search():
+    ip = config.LOCAL_DNS_HOST
+    return render_template('search.html', ip=ip)
+
+
+# @app.route('/_stuff', methods=['GET'])
+# def stuff():
+#     global heading
+#     if request.method == 'GET':
+#         if heading:
+#             print heading['value']
+#             return heading['value']
+#         else:
+#             blank = ""
+#             return blank
+
 
 # @app.route('/events', methods=['GET', 'POST'])
 # def events():
-	# global heading
-	# heading = request.json
-	# print heading['value']
-	# return jsonify({"content":heading})
-
-
+#     global heading
+#     heading = request.json
+#     print heading['value']
+#     return jsonify({"content":heading})
 
 if __name__ == "__main__":
-	app.run(debug=True, host=str(config.LOCAL_DNS_HOST))
+    app.run(debug=True, host=str(config.LOCAL_DNS_HOST))
